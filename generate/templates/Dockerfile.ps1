b7d229530e75a5a1793a5e8fc8bbdfd8174ddbba
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-04-06 19:14:15 +0000

    Chore: Cleanup `Dockerfile`

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -6,9 +6,8 @@ RUN set -eux; \
     gem install imap-backup -v $( $VARIANT['_metadata']['package_version'] ); \
     imap-backup help > /dev/null
 
-# Make the data directory a volume
 WORKDIR /root
-VOLUME ["/root/.imap-backup"]
+VOLUME /root/.imap-backup
 
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod +x /docker-entrypoint.sh
