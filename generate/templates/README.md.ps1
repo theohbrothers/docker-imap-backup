ec55627c8172bd5671a3495f4e21d8a6496055ec
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2024-03-16 09:20:02 +0000

    Docs (readme): Update GitHub CI badge
    Signed-off-by: Leonard Jonathan Oh <leonardjonathanoh@live.com>

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -1,7 +1,7 @@
 @"
 # docker-imap-backup
 
-[![github-actions](https://github.com/theohbrothers/docker-imap-backup/workflows/ci-master-pr/badge.svg)](https://github.com/theohbrothers/docker-imap-backup/actions)
+[![github-actions](https://github.com/theohbrothers/docker-imap-backup/actions/workflows/ci-master-pr.yml/badge.svg?branch=master)](https://github.com/theohbrothers/docker-imap-backup/actions/workflows/ci-master-pr.yml)
 [![github-release](https://img.shields.io/github/v/release/theohbrothers/docker-imap-backup?style=flat-square)](https://github.com/theohbrothers/docker-imap-backup/releases/)
 [![docker-image-size](https://img.shields.io/docker/image-size/theohbrothers/docker-imap-backup/latest)](https://hub.docker.com/r/theohbrothers/docker-imap-backup)
 
