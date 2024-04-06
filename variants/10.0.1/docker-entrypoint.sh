ac344760bc351486cf2ab9f21abf3ad82c94a723
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2024-04-06 07:27:07 +0000

    Fix: Fix `docker-entrypoint.sh` printing passed arguments

--- variants/10.0.1/docker-entrypoint.sh
+++ variants/10.0.1/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -eu
 
 if [ $# -gt 0 ]; then
     SUBCOMMANDS=$( imap-backup help | grep -E '^\s*imap-backup' | awk '{print $2}' | sort -n | uniq )
-    if echo "$SUBCOMMANDS" | grep "^$1$"; then
+    if echo "$SUBCOMMANDS" | grep "^$1$" > /dev/null; then
         exec imap-backup "$@"
     fi
 else
